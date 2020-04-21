1b43bbc4c61f5ec1827c98c173e663c23479ee22
Ben Bryant <brbryant2639@gmail.com>
2020-04-21 16:00:46 -0500

    initial commit without config

---
+++ src/server/config/production.ts
@@ -0,0 +1,9 @@
+export default {
+    mysql: {
+        host: process.env.DB_HOST,
+        port: 3306,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_SCHEMA
+    }
+}
